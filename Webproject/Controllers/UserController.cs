44c8c39cb90e04822071662b8331f2ab731c3755
JulijaFinaskina <57600768+JulijaFinaskina@users.noreply.github.com>
2019-11-11 10:03:02 +0200

    Add files via upload

---
+++ Webproject/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Webproject.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            var manager = new UserManager();
+            manager.Seed();
+            var users = manager.GetAll();
+
+            return View(users);
+        }
+    }
+}
