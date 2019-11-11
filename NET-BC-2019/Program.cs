b86b8d4e2c1727a99b36ed2d78b4226b72d4a28e
JulijaFinaskina <57600768+JulijaFinaskina@users.noreply.github.com>
2019-11-11 08:46:14 +0200

    Add files via upload

---
+++ NET-BC-2019/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET_BC_2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
