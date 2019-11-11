27e9f8e2a0cc6f55024715a2b9d0278cba44965a
JulijaFinaskina <57600768+JulijaFinaskina@users.noreply.github.com>
2019-11-11 08:32:00 +0200

    Add files via upload

---
+++ ConsoleHelpers/IntegerReader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleHelpers
+{
+    public class DateReader : BaseConsoleReader<DateTime>
+    {
+        protected override bool CheckValue(string input, out DateTime value)
+        {
+            return DateTime.TryParse(input, out value);
+        }
+    }
+}
+
+
