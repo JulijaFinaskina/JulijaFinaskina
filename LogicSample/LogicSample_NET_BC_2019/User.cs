69dc17e2937e4a6a961968541a6ce2f71a3d79fa
JulijaFinaskina <57600768+JulijaFinaskina@users.noreply.github.com>
2019-11-11 11:50:05 +0200

    Add files via upload

---
+++ LogicSample/LogicSample_NET_BC_2019/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicSample_NET_BC_2019
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
