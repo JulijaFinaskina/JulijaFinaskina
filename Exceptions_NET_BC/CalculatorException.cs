83bff77424155def320c62e1015875825b29416f
JulijaFinaskina <57600768+JulijaFinaskina@users.noreply.github.com>
2019-11-11 11:36:08 +0200

    Add files via upload

---
+++ Exceptions_NET_BC/CalculatorException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions_NET_BC
+{
+    class CalculatorException : Exception
+    {
+        public CalculatorException(string message)
+            : base(message)
+        {
+        }
+    }
+
+}
